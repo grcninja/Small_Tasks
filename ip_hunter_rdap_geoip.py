54400182f8970d9f8cc97135b30c54e49a01d74f
Tazz <grcninja@users.noreply.github.com>
2016-04-26 21:21:08 -0500

    Update ip_hunter_rdap_geoip.py
    Added 3 fields "contact_info_link[1-3]" 
    - Why? to reflect the source links where contact data is pulled from because some people are assholes when you try to contact them, and they tell you to go update "your" whois db.  So now you can give them the source of their problem so they know how you reached them and who to complain to.
    
    Added check for NONE value on MaxMind data, now default "UNK" will populate the field, no more blanks
    
    Added writeline to error handling so that every IP you check will have a corresponding returned line in your output, even if there was an error returned while trying to pull the whois data for it

--- ip_hunter_rdap_geoip.py
+++ ip_hunter_rdap_geoip.py
@@ -8,9 +8,9 @@ it requires you to have a GeoIP2-City database from MaxMind.
 
 If you do not wish to purchase this, consider using the shodan package instead as you can pull geo data from there as well
 
-Dependency on 
-1) geoip2
-2) ipwhois
+Dependency on geoip2 package
+
+You can get a free 
 '''
 import csv
 import datetime
@@ -32,28 +32,41 @@ def newline_clean(original):
          cleaned = original#shame on me for this
     return cleaned
 
+def phone_clean(phone):
+    try:
+        just_phone = phone.strip("\n\r\t abcdefghijklmnopqrstuvwxyz:;")
+    except:
+        just_phone = phone
+    return just_phone
+
+
 def getMaxMind(ip_address):
     ip_addy = ip_address
     geo_ip = {"MaxMind_ip_iso_code":"UNK","MaxMind_ip_country":"UNK","MaxMind_ip_state":"UNK","MaxMind_ip_city":"UNK","MaxMind_ip_zipcode":"UNK"}
     #open GeoIP2 data reader
-    reader = geoip2.database.Reader("/path/to/Maxmind/GeoIP2-City.mmdb")
+    reader = geoip2.database.Reader("C:/Users/Tazz/Desktop/MyPy/Maxmind_GeoIP2-City.mmdb")
     try:
         ip = reader.city(ip_addy)
-        geo_ip["MaxMind_ip_iso_code"] = (newline_clean(ip.country.iso_code)) #US
-        geo_ip["MaxMind_ip_country"] = (newline_clean(ip.country.name)) #'United States'
-        geo_ip["MaxMind_ip_state"] = (newline_clean(ip.subdivisions.most_specific.name)) #'Minnesota'
-        geo_ip["MaxMind_ip_city"] = (newline_clean(ip.city.name)) #'Minneapolis'
-        geo_ip["MaxMind_ip_zipcode"] = (newline_clean(ip.postal.code)) #'55455'
+        if ip.country.iso_code is not None:
+            geo_ip["MaxMind_ip_iso_code"] = (newline_clean(ip.country.iso_code)) #US
+        if ip.country.name is not None:
+            geo_ip["MaxMind_ip_country"] = (newline_clean(ip.country.name)) #'United States'
+        if ip.subdivisions.most_specific.name is not None:
+            geo_ip["MaxMind_ip_state"] = (newline_clean(ip.subdivisions.most_specific.name)) #'Minnesota'
+        if ip.city.name is not None:
+            geo_ip["MaxMind_ip_city"] = (newline_clean(ip.city.name)) #'Minneapolis'
+        if ip.postal.code is not None:
+            geo_ip["MaxMind_ip_zipcode"] = (newline_clean(ip.postal.code)) #'55455'
     except:
         print("Unexpected Error getting MaxMind GeoIP data for: "+ip_addy)
     reader.close()
     return geo_ip
 
 dt = datetime.date.today()
-input_path = "/path/to/myinput/"
-input_name = "$inputfilename.txt"
+input_path = "C:/Users/Tazz/Desktop/MyPy/_myinput/"
+input_name = "ips.txt"
 input_file_name = os.path.join(input_path, input_name)
-output_path = "/path/to/myoutput/"
+output_path = "C:/Users/Tazz/Desktop/MyPy/_myoutput/"
 output_name = ("IP_details_"+str(dt)+".csv")
 output_file_name = os.path.join(output_path, output_name)
 process_file_name = os.path.join(input_path,"cleaned_input.txt")
@@ -74,25 +87,48 @@ print(str(work)+" ips to process\n")
 with open(process_file_name,"r") as fin, open(output_file_name,"w",newline='',encoding="ascii", errors="ignore") as csvfile:
 
     #set up CSV file
-    fieldnames = ["ip_addr" , "handle" , "asn" , "asn_country_code" , "network_cidr" , "contact_address" , "contact_email" , "contact_phone" , "contact_name" , "contact_title" , "contact_role" , "MaxMind_ip_iso_code" , "MaxMind_ip_country" , "MaxMind_ip_state" , "MaxMind_ip_city" , "MaxMind_ip_zipcode"]
+    fieldnames = ["ip_addr" ,
+                  "handle" ,
+                  "asn" ,
+                  "asn_country_code" ,
+                  "network_cidr" ,
+                  "contact_address" ,
+                  "contact_email" ,
+                  "contact_phone" ,
+                  "contact_name" ,
+                  "contact_title" ,
+                  "contact_role" ,
+                  "contact_info_link1" ,
+                  "contact_info_link2" ,
+                  "contact_info_link3" ,
+                  "MaxMind_ip_iso_code" ,
+                  "MaxMind_ip_country" ,
+                  "MaxMind_ip_state" ,
+                  "MaxMind_ip_city" ,
+                  "MaxMind_ip_zipcode"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-    #set up variables
-    addr = "UNK"
-    handle = "UNK"
-    asn = "UNK"
-    asn_country_code = "UNK"
-    network_cidr = "UNK"
-    contact_address = "UNK"
-    contact_email = "UNK"
-    contact_phone = "UNK"
-    contact_name = "UNK"
-    contact_title = "UNK"
-    contact_role = "UNK"
+
     
     for line in fin:
+
+        #set up variables
+        handle = "None"
+        asn = "None"
+        asn_country_code = "None"
+        network_cidr = "None"
+        contact_address = "None"
+        contact_email = "None"
+        contact_phone = "None"
+        contact_name = "None"
+        contact_title = "None"
+        contact_role = "None"
+        contact_info_link1 = "None"
+        contact_info_link2 = "None"
+        contact_info_link3 = "None"
         addr = line.rstrip().strip()
+        
         print("Processing "+str(addr)+" "+str(work)+" more IP's to go")
 
         #Get GeoIP data from MaxMind database
@@ -111,6 +147,15 @@ with open(process_file_name,"r") as fin, open(output_file_name,"w",newline='',en
             asn = (newline_clean(results['asn']))
             asn_country_code = (newline_clean(results['asn_country_code']))
             network_cidr = (newline_clean(results['network']['cidr']))
+            tmp_links = (newline_clean(results['network']['links']))
+            for x in range(len(tmp_links)):
+                if x == 0:
+                    contact_info_link1 = tmp_links[0]
+                elif x == 1:
+                    contact_info_link2 = tmp_links[1]
+                elif x == 2:
+                    contact_info_link3 = tmp_links[2]
+
             #Entry identifier - this is the Internet Registry Number, known as the handle Ex:ZG39-ARIN for Google
             for object_key, object_dict in results['objects'].items():
                 handle = str(object_key)
@@ -120,32 +165,32 @@ with open(process_file_name,"r") as fin, open(output_file_name,"w",newline='',en
                         #Address - first result only
                         tmp_add = results['objects'][k]['contact']['address']
                         if tmp_add is not None:
-                            contact_address = (newline_clean(tmp_add[0]['value']))
+                            contact_address = str(newline_clean(tmp_add[0]['value']))
 
                         #Email - first result only
                         tmp_em = results['objects'][k]['contact']['email']
                         if tmp_em is not None:
-                            contact_email = (newline_clean(tmp_em[0]['value']))
+                            contact_email = str(newline_clean(tmp_em[0]['value']))
 
                         #Phone - first result only
                         tmp_ph = results['objects'][k]['contact']['phone']
                         if tmp_ph is not None:
-                            contact_phone = (newline_clean(tmp_ph[0]['value']))
+                            contact_phone = str(phone_clean(tmp_ph[0]['value']))
 
                         #Name - string result
                         tmp_nm = results['objects'][k]['contact']['name']
                         if tmp_nm is not None:
-                            contact_name = (newline_clean(tmp_nm))
+                            contact_name = str(newline_clean(tmp_nm))
 
                         #Title - string result
                         tmp_ti = results['objects'][k]['contact']['title']
                         if tmp_ti is not None:
-                            contact_title = (newline_clean(tmp_ti))
+                            contact_title = str(newline_clean(tmp_ti))
 
                         #Role - string result
                         tmp_ro = results['objects'][k]['contact']['role']
                         if tmp_ro is not None:
-                            contact_role = (newline_clean(tmp_ro))
+                            contact_role = str(newline_clean(tmp_ro))
 
             #Create Entry
             writer.writerow({"ip_addr" : addr,
@@ -159,6 +204,9 @@ with open(process_file_name,"r") as fin, open(output_file_name,"w",newline='',en
                              "contact_name" : contact_name,
                              "contact_title" : contact_title,
                              "contact_role" : contact_role,
+                             "contact_info_link1" : contact_info_link1,
+                             "contact_info_link2" : contact_info_link2,
+                             "contact_info_link3" : contact_info_link3,
                              "MaxMind_ip_iso_code" : MaxMind_ip_iso_code,
                              "MaxMind_ip_country" : MaxMind_ip_country,
                              "MaxMind_ip_state" : MaxMind_ip_state,
@@ -167,6 +215,25 @@ with open(process_file_name,"r") as fin, open(output_file_name,"w",newline='',en
 
         except Exception as e:
             #FWIW, there's much better ways to log errors, but I don't need it for what I am doing
+            writer.writerow({"ip_addr" : addr,
+                 "handle" : "Error while pulling data",
+                 "asn" : "-",
+                 "asn_country_code" : "-",
+                 "network_cidr" : "-",
+                 "contact_address" : "-",
+                 "contact_email" : "-",
+                 "contact_phone" : "-",
+                 "contact_name" : "-",
+                 "contact_title" : "-",
+                 "contact_role" : "-",
+                 "contact_info_link1" : "-",
+                 "contact_info_link2" : "-",
+                 "contact_info_link3" : "-",
+                 "MaxMind_ip_iso_code" : "-",
+                 "MaxMind_ip_country" : "-",
+                 "MaxMind_ip_state" : "-",
+                 "MaxMind_ip_city" : "-",
+                 "MaxMind_ip_zipcode": "-"})
             with open(errors_file,"a") as ferr:
                 ferr.write("Unexpected error getting WhoIs info for: "+addr)
                 ferr.write("\n")
